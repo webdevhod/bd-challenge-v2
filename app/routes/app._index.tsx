8e19117c2b14f507fd2425dca9fb8028ce753773
Daniel Ho <webdevhod@gmail.com>
2025-02-25 18:49:26 -0800

    Product review placeholder

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,139 +1,34 @@
-import {
-  Page,
-  Layout,
-  Text,
-  Card,
-  BlockStack,
-  Link,
-  InlineStack,
-  InlineCode,
-} from "@shopify/polaris";
+import { Page, Layout, BlockStack, MediaCard } from "@shopify/polaris";
 
 export default function Index() {
   return (
-    <Page title="Reviewer 9000">
+    <Page title="Product Reviews">
       <BlockStack gap="500">
         <Layout>
           <Layout.Section>
-            <Card>
-              <BlockStack gap="500">
-                <BlockStack gap="200">
-                  <Text as="h2" variant="headingMd">
-                    Congrats on creating a new Shopify product reviewing app! 🎉
-                  </Text>
-                  <Text variant="bodyMd" as="p">
-                    This embedded app template uses{" "}
-                    <Link
-                      url="https://shopify.dev/docs/apps/tools/app-bridge"
-                      target="_blank"
-                      removeUnderline
-                    >
-                      App Bridge
-                    </Link>
-                    . Please review the{" "}
-                    <InlineCode>/extensions/review-embed</InlineCode> directory
-                    and the{" "}
-                    <InlineCode>/routes/api.public.$/route.tsx</InlineCode>{" "}
-                    directory before getting started.
-                  </Text>
-                </BlockStack>
-              </BlockStack>
-            </Card>
-          </Layout.Section>
-          <Layout.Section variant="oneThird">
-            <BlockStack gap="500">
-              <Card>
-                <BlockStack gap="200">
-                  <Text as="h2" variant="headingMd">
-                    App template specs
-                  </Text>
-                  <BlockStack gap="200">
-                    <InlineStack align="space-between">
-                      <Text as="span" variant="bodyMd">
-                        Framework
-                      </Text>
-                      <Link
-                        url="https://remix.run"
-                        target="_blank"
-                        removeUnderline
-                      >
-                        Remix
-                      </Link>
-                    </InlineStack>
-                    <InlineStack align="space-between">
-                      <Text as="span" variant="bodyMd">
-                        Database
-                      </Text>
-                      <Link
-                        url="https://www.prisma.io/"
-                        target="_blank"
-                        removeUnderline
-                      >
-                        Prisma
-                      </Link>
-                    </InlineStack>
-                    <InlineStack align="space-between">
-                      <Text as="span" variant="bodyMd">
-                        Interface
-                      </Text>
-                      <span>
-                        <Link
-                          url="https://polaris.shopify.com"
-                          target="_blank"
-                          removeUnderline
-                        >
-                          Polaris
-                        </Link>
-                        {", "}
-                        <Link
-                          url="https://shopify.dev/docs/apps/tools/app-bridge"
-                          target="_blank"
-                          removeUnderline
-                        >
-                          App Bridge
-                        </Link>
-                      </span>
-                    </InlineStack>
-                    <InlineStack align="space-between">
-                      <Text as="span" variant="bodyMd">
-                        API
-                      </Text>
-                      <Link
-                        url="https://shopify.dev/docs/api/admin-graphql"
-                        target="_blank"
-                        removeUnderline
-                      >
-                        GraphQL API
-                      </Link>
-                    </InlineStack>
-                    <InlineStack align="space-between">
-                      <Text as="span" variant="bodyMd">
-                        Extension
-                      </Text>
-                      <Link
-                        url="https://shopify.dev/docs/apps/build/online-store/theme-app-extensions"
-                        target="_blank"
-                        removeUnderline
-                      >
-                        Theme App Extension
-                      </Link>
-                    </InlineStack>
-                    <InlineStack align="space-between">
-                      <Text as="span" variant="bodyMd">
-                        Proxy
-                      </Text>
-                      <Link
-                        url="https://shopify.dev/docs/api/shopify-app-remix/v1/authenticate/public/app-proxy"
-                        target="_blank"
-                        removeUnderline
-                      >
-                        Shopify App Proxy
-                      </Link>
-                    </InlineStack>
-                  </BlockStack>
-                </BlockStack>
-              </Card>
-            </BlockStack>
+            <MediaCard
+              title="Business Suit"
+              primaryAction={{
+                content: "Edit",
+                onAction: () => {},
+              }}
+              description="Rating: 4.5/5. This business suit is fantastic! The fit is perfect, and the
+                material is high quality. I feel confident and professional
+                every time I wear it. Highly recommend!"
+              popoverActions={[{ content: "Dismiss", onAction: () => {} }]}
+              size="small"
+            >
+              <img
+                alt="Item 1"
+                width="100%"
+                height="100%"
+                style={{
+                  objectFit: "cover",
+                  objectPosition: "center",
+                }}
+                src="https://burst.shopifycdn.com/photos/business-woman-smiling-in-office.jpg?width=1850"
+              />
+            </MediaCard>
           </Layout.Section>
         </Layout>
       </BlockStack>
